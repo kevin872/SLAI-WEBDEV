84ca58e829f43f537a28c386d8f2baf47f7f7acf
Kevin Reynoso <kevinr63753@gmail.com>
2024-07-16 11:35:17 -0500

    day2

--- day2/app.py
+++ day2/app.py
@@ -1,11 +1,12 @@
 from data_exp import read_csv
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    data = read_csv("pokemon.csv")
+    return render_template("index.html", data=data)
 
 @app.route("/<int:num>")
 def details(num):
